04b23f9f6e42dfe3c6578e1a5aa8c44308e87961
Kealiwyn <sublame1992@gmail.com>
2021-04-12 14:26:13 -0400

    Add project files.

---
+++ PrimeMinisters/PrimeMinister.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeMinisters
+{
+    class PrimeMinister
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Term { get; set; }
+        public string Party { get; set; }
+    }
+}
